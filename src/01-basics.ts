3c5098d99e8324dc258c97f942dd72f2986936f6
Airidas Balkus <147723109+AiridasBalkus@users.noreply.github.com>
2026-09-17 14:55:39 +0100

    Refactor - Add type declaration file; disable redundant console logs.

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -1,8 +1,7 @@
-interface Friend {
-    name: string;
-    phone: string;
-    age: number;
-}
+import {Friend, Colleague, ColleagueHistory } from './myTypes'
+
+
+
 const friend1 = {
   name: "Paul Fleming",
   phone: "087-12345",
@@ -15,17 +14,9 @@ const friend2 = {
   age: 31,
 };
 
-const friends = [friend1, friend2];
-console.log(friends[1]);
+export const friends = [friend1, friend2];
+// console.log(friends[1]);
 
-interface Colleague {
-    name: string;
-    department: string;
-    contact: {
-        email: string;
-        extension: number;
-    };
-}
 
 const colleague1 = {
   name: "Ralph Graham",
@@ -53,15 +44,10 @@ const colleague3 = {
     extension: 125,
   },
 };
-interface ColleagueHistory {
-  current: Colleague[],
-  former: Colleague[]
-}
+
 export const colleagues : ColleagueHistory = {
   current: [colleague1, colleague2, colleague3],
   former: [],
 };
 
-
-
-console.log(colleagues.current[0]);
+// console.log(colleagues.current[0]);
